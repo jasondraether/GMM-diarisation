a96521aa85b024a682c5140fe8286c13fea5a57c
JRaether16 <jasondraether@gmail.com>
2019-06-12 20:35:24 -0500

    Added main program for nn to test, need to setup data

--- nn.py
+++ nn.py
@@ -1,5 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
+from keras import regularizers, optimizers
+import AudioProcessor
 
 '''
 Neural network code for
@@ -7,13 +9,15 @@ audio-learning
 '''
 class NeuralNetwork:
 
-    def __init__(self, kernel_size, activation, dropout, pool_size, input_shape, output_labels):
+    def __init__(self, kernel_size, activation, dropout, pool_size, input_shape, output_labels, learning_rate, decay):
         self.kernel_size = kernel_size
         self.activation = activation
         self.dropout = dropout
         self.pool_size = pool_size
         self.input_shape = input_shape # Shape of spectrogram is (129, 196)
         self.output_labels = output_labels
+        self.learning_rate = learning_rate
+        self.decay = decay
 
     def create_model(self):
         model = Sequential()
@@ -25,7 +29,19 @@ class NeuralNetwork:
             input_shape=self.input_shape))
         model.add(Dropout(self.dropout))
         model.add(Flatten())
-        model.add(Dense(32, self.activation))
+        model.add(Dense(32))
         model.add(Dense(1, activation='softmax'))
+        model.compile(optimizers.rmsprop(lr=self.learning_rate, decay=self.decay), loss="categorical_crossentropy",metrics=["accuracy"])
 
         self.model = model
+
+if __name__ == '__main__':
+
+    nn = NeuralNetwork((3,3), 'relu', 0.5, (2,2), (129, 196, 1), ['Matt', 'Ryan'], 0.0005, 1e-6)
+    nn.create_model()
+    nn.model.summary()
+
+    audio_proc = AudioProcessor()
+
+    # Put spectrograms from labeled .wavs into
+    # a training set, shuffle, and train
